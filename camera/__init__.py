e45b837bf8aa5220436c76da1e0dc2f7a027475c
Benjamin <bcsherma@ucsc.edu>
2018-10-08 22:18:53 -0700

    importing all modules that are currently implemented

--- camera/__init__.py
+++ camera/__init__.py
@@ -4,6 +4,11 @@ __init__.py
 Import modules to the toplevel package
 """
 
+from . import sat
 from . import hmqc
 from . import noes
+from . import params
+from . import ground
 from . import network
+from . import structures
+from . import symmetrize
